1bc79022533c84285a90a48dffe0a20f0621b706
dwiYustiana <dwi.yustian@gmail.com>
2016-06-07 10:23:37 +0700

    nyoba edit

--- assets/js/app-validate.js
+++ assets/js/app-validate.js
@@ -466,4 +466,78 @@ $(function () {
         
         return false;
     }
+
+    // Job Analyst - Edit
+    if (document.getElementById('page_job_analyst_edit') != null) {
+        var e=$("#form_job_analyst_edit"),
+            r=$(".alert-danger", e),
+            i=$(".alert-success", e);
+        console("masuk");  
+        e.validate( {
+            errorElement:"span",
+            errorClass:"help-block help-block-error",
+            focusInvalid:!1,
+            ignore:"",
+            rules: {
+                name: {
+                    required: true,
+                    remote: {
+                        url: newPathname + "check_job_analyst_name",
+                        type: "post",
+                        data: {
+                            email: function() {
+                                return $("#name").val();
+                            }
+                        }
+                    }
+                }
+            },
+            messages: {
+                name: {
+                    required:"Please insert job analyst name.",
+                    remote: "Name already exist"
+                }
+            },
+            invalidHandler:function(e, t) {
+                i.hide(), r.show(), App.scrollTo(r, -200)
+            },
+            errorPlacement:function(e, r) {
+                r.is(":checkbox")?e.insertAfter(r.closest(".md-checkbox-list, .md-checkbox-inline, .checkbox-list, .checkbox-inline")): r.is(":radio")?e.insertAfter(r.closest(".md-radio-list, .md-radio-inline, .radio-list,.radio-inline")): e.insertAfter(r)
+            },
+            highlight:function(e) {
+                $(e).closest(".form-group").addClass("has-error")
+            },
+            unhighlight:function(e) {
+                $(e).closest(".form-group").removeClass("has-error")
+            },
+            success:function(e) {
+                e.closest(".form-group").removeClass("has-error")
+            },
+            submitHandler:function(e) {
+                i.show(), r.hide();
+                $('.modal-title').text('Please wait...');
+                $('.modal-body').html('<i class="fa fa-spinner fa-spin" style="font-size: 34px;"></i>');
+                $('.modal-dialog').addClass('modal-sm');
+                $('#myModal').modal('show');
+                $.ajax(
+                {
+                    type: "POST",
+                    url: e.action,
+                    data: $(e).serialize(), 
+                    cache: false,
+                    success: function(data)
+                    {
+                        var response = $.parseJSON(data);
+                        if (response.type == 'success')
+                        {
+                            setTimeout("location.href = '"+response.location+"'",2000);
+                        }
+                    }
+                });
+            }
+        });
+        
+        return false;
+    }
+   
 });
